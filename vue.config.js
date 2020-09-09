0a47de8bc2a6bda8a2698407b0d085a6284f2539
richarne <richarne@uio.no>
2020-09-09 09:18:29 +0200

    update publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,4 @@
 // vue.config.js
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.mp3$/,
-        loader: "url-loader",
-        options: {
-          limit: 10000,
-          name: utils.assetsPath("img/[name].[hash:7].[ext]"),
-        },
-      },
-    ],
-  },
-};
+  publicPath: '',
+}
