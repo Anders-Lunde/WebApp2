76a4e594dcc86c8640617d998e8fe27097e0bd0a
Anders Lunde <andelu@uio.no>
2020-09-25 10:59:30 +0200

    removed most workbox code

--- workbox-config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  globDirectory: "public/",
-  globPatterns: [
-    "**/*.{js,json,ico,html,md,vue,png,mp3,jpg,gif,css,mp4,ts,lock}",
-  ],
-  swDest: "./public/sw.js",
-  skipWaiting: true,
-  clientsClaim: true,
-};
