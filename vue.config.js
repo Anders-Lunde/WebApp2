4ac24cb37688fa64e79d89054a8b313ec19e9b79
Anders Lunde <andelu@uio.no>
2020-09-25 16:06:41 +0200

    Adding pwa options

--- vue.config.js
+++ vue.config.js
@@ -11,18 +11,17 @@ module.exports = {
   },
   // ...other vue-cli plugin options...
   pwa: {
-    name: "Anders App",
     themeColor: "#4DBA87",
     msTileColor: "#000000",
     appleMobileWebAppCapable: "yes",
     appleMobileWebAppStatusBarStyle: "black",
+    manifestOptions: { display: "standalone" },
 
     // configure the workbox plugin
     workboxPluginMode: "InjectManifest",
     workboxOptions: {
       // swSrc is required in InjectManifest mode.
       swSrc: "src/registerServiceWorker.ts",
-      // ...other Workbox options...
     },
   },
 };
